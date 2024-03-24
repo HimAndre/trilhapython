170bf73f10cb8658f032513830a508a9e132f0bd
HimAndre <handrelukas@gmail.com>
2024-03-24 01:28:37 -0300

    Estudo do dia

---
+++ dict/dict_1.py
@@ -0,0 +1,9 @@
+carro ={"marca": "Fiat", "ano": 1999}
+
+carro = dict(marca="Fiat", ano=1999)
+
+carro["tipo"] = "Popular"
+
+#são essas as formas nas quais podemos utilizar "call" dict
+
+print(carro)
---
+++ keys.py
@@ -0,0 +1,9 @@
+carro ={"marca": "Fiat", "ano": 1999}
+
+resultado = carro.keys()
+print(resultado)
+
+
+novos_carros = {"a":100, 1: "teste","b":"python"}
+print(novos_carros.keys())
+
