ccdbeb446a3de173e353f4317cc02c2d1e1d5052
HimAndre <handrelukas@gmail.com>
2024-03-25 21:58:43 -0300

    parametros

---
+++ Funçoes/paramentos_so_por_posição.py
@@ -0,0 +1,4 @@
+def cria_carro(modelo,ano,placa, /, marca, motor,combustível):
+    print(modelo, ano, placa, marca, motor, combustível)
+
+cria_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustível="gasolina")
