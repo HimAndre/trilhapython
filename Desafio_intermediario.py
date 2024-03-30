33542c1234eb3922c3520ffcaa0e5b8e00909e07
HimAndre <handrelukas@gmail.com>
2024-03-29 22:38:59 -0300

    Desafio01

---
+++ Desafio_intermediario.py
@@ -0,0 +1,14 @@
+c = int(input())
+for i in range(c):
+    
+    i = int(input())
+
+    if i <= 8000:
+        print("Inseto")
+    else:
+        print("Mais de 8000")
+
+
+#apos muitas tentativas efetivei com sucesso esse desafio
+#sistema de desafio da dio não é fluido
+        
