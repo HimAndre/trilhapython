ac219d0895859644fac3e674be7bce13df4f09f8
HimAndre <handrelukas@gmail.com>
2024-04-04 20:37:53 -0300

    mini desafio bicicleta

--- POO/mini_desafio.py
+++ POO/mini_desafio.py
@@ -7,11 +7,12 @@ vendida. Uma bicicleta pode: buzinar, parar e correr.
 adicione esses comportamentos!!!! '''
 
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor)
+    def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo 
         self.ano = ano 
         self.valor = valor
+
 
     def buzina(self):
         print('BIIIIIIIIIIIIIIII')
@@ -22,4 +23,20 @@ class Bicicleta:
     def correr(self):
         print('eu sou o flash!')
         
-        
+    # def __str__(self):
+    #     return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano},valor={self.valor}"
+    
+    def __str__(self):
+         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+    
+b1 = Bicicleta('vermelha','monark', 1999, 1500)
+b1.buzina()
+b1.parar()
+b1.correr()
+print(b1.cor, b1.modelo,b1.valor,b1.ano)
+
+b2 = Bicicleta('Verder', 'monark',2000, 189)
+print(b2)
+b2.correr()
+
+
