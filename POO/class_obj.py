973eddf7c61464d678875b73fcb21bd883b1ae4a
HimAndre <handrelukas@gmail.com>
2024-04-03 21:47:12 -0300

    Poo_class_obj.py

---
+++ POO/class_obj.py
@@ -0,0 +1,25 @@
+class Cachorro:
+    def __init__(self, nome, cor,acordado=True):
+        self.nome = nome 
+        self.cor = cor 
+        self.acordado = acordado
+
+    def latir(self):
+        print('auau')
+    
+    def dormir(self):
+        self.acordado = False  
+        print('zzzzzz')
+
+#Exemplo de class
+
+cao_1 = Cachorro('chappie','amarelo',False)
+cao_2 = Cachorro('Aladim','branco e preto')
+
+cao_1.latir()
+
+print(cao_2.acordado)
+cao_2.dormir()
+print(cao_2.acordado)
+
+#exemplos de obj 
---
+++ POO/mini_desafio.py
@@ -0,0 +1,25 @@
+''' 
+primeiro progama POO
+Joao tem uma bicicleta e gostaria de registrar as
+vendas de suas bicicletas. Crie um progama onde 
+Joao informe: cor, modelo, ano e valor da bicicleta
+vendida. Uma bicicleta pode: buzinar, parar e correr.
+adicione esses comportamentos!!!! '''
+
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor)
+        self.cor = cor
+        self.modelo = modelo 
+        self.ano = ano 
+        self.valor = valor
+
+    def buzina(self):
+        print('BIIIIIIIIIIIIIIII')
+
+    def parar(self):
+        print('parou')
+
+    def correr(self):
+        print('eu sou o flash!')
+        
+        
