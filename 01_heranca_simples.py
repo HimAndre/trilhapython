7ae4005c7c7d0e18dc54ea04717d4df6155575a9
HimAndre <handrelukas@gmail.com>
2024-04-10 21:56:28 -0300

    estudos

--- 01_heranca_simples.py
+++ 01_heranca_simples.py
@@ -1,32 +1,37 @@
 class Veiculo:
-    def __init__ (self, cor, placa , numero_rodas):
+    def __init__(self, cor, placa, numero_rodas):
         self.cor = cor
-        self.placa = placa 
+        self.placa = placa
         self.numero_rodas = numero_rodas
 
     def ligar_motor(self):
-            print("ligando o MOTOR")
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
 class Motocicleta(Veiculo):
     pass
 
+
 class Carro(Veiculo):
     pass
 
+
 class Caminhao(Veiculo):
-    def __init__(self, carregado):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
         self.carregado = carregado
-    
+
     def esta_carregado(self):
-        print(f'{"sim" if self.carregado else "nao" } estou carregado')
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
 
-# moto = Motocicleta('preta','abc-123', 2)
-# moto.ligar_motor()
 
-# carro = Carro("branco", "acb-123", 4)
-# carro.ligar_motor()                
+moto = Motocicleta("preta", "abc-1234", 2)
+carro = Carro("branco", "xde-0098", 4)
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
 
-caminhao = Caminhao('roxa', "das-123", 8, True)
-caminhao.ligar_motor()
-caminhao.esta_carregado()
+print(moto)
+print(carro)
+print(caminhao)
